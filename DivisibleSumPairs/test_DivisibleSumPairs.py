c7e106c0538f4923c3dcef0b56c4c1463a83bd8e
Meanthadar Promsutthiphan <39612742+meanthadar-p@users.noreply.github.com>
2020-06-13 00:48:33 +0700

    [HKRK-2] Drawing Book (#2)
    * set up env to run pytest
    
    * practice and unit tests
    
    * refactor files name
    
    Co-authored-by: mpromsutthip <meanthadar.promsutthiphan@agoda.com>

---
+++ DivisibleSumPairs/test_DivisibleSumPairs.py
@@ -0,0 +1,11 @@
+from DivisibleSumPairs import divisible_sum_pairs
+import pytest
+
+
+@pytest.mark.parametrize("data, expected",
+                         [
+                             ((6, 3, [1, 3, 2, 6, 1, 2]), 5)
+                         ])
+def test_normal_case(data, expected):
+    result = divisible_sum_pairs(data[0], data[1], data[2])
+    assert result == expected
