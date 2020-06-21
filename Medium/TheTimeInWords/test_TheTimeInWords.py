c7f5223d427a386f9b08b995bcd37003eb3b5dc6
mpromsutthip <meanthadar.promsutthiphan@agoda.com>
2020-06-21 23:34:12 +0700

    add TheTimeInTords

---
+++ Medium/TheTimeInWords/test_TheTimeInWords.py
@@ -0,0 +1,19 @@
+import pytest
+from TheTimeInWords import the_time_in_words
+
+
+@pytest.mark.parametrize("h, m, expected",
+                         [
+                             (5, 00, "five o' clock"),
+                             (5, 01, "one minute past five"),
+                             (5, 10, "ten minutes past five"),
+                             (5, 15, "quarter past five"),
+                             (5, 28, "twenty eight minutes past five"),
+                             (5, 30, "half past five"),
+                             (5, 40, "twenty minutes to six"),
+                             (5, 45, "quarter to six"),
+                             (5, 47, "thirteen minutes to six")
+                         ])
+def test_the_time_in_world(h, m, expected):
+    result = the_time_in_words(h, m)
+    assert result == expected
